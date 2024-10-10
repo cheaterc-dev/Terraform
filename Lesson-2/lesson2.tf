2a217e0f69020ace19c8065d174af267792ddca0
cheaterc-dev <99823559+cheaterc-dev@users.noreply.github.com>
2024-10-10 18:09:39 +0300

    Update lesson2.tf

--- Lesson-2/lesson2.tf
+++ Lesson-2/lesson2.tf
@@ -1,26 +1,44 @@
+#----------------------------------
+# My terrafrom
+#Build my server
+#
+#----------------------------------
+
 provider "aws" {
     access_key = 
     secret_key = 
+
     region = "eu-central-1"
 
 }
-
-resource "aws_instance" "my_ubuntu" {
-    ami = "ami-0084a47cc718c111a"
-    instance_type = "t2.micro"
-    tags = {
-        Name= "my ubuntu server1"
-        Owner = "Denis"
-        Project = "Terraform"
-    }
+resource "aws_instance" "my_web" {
+  ami = "ami-0084a47cc718c111a"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.my_webserver.id]
+ 
 }
 
-resource "aws_instance" "my_ubuntu1" {
-    ami = "ami-0084a47cc718c111a"
-    instance_type = "t2.micro"
-    tags = {
-        Name= "my ubuntu server2"
-        Owner = "Denis"
-        Project = "Terraform"
-    }
-}
+resource "aws_security_group" "my_webserver" {
+  name        = "Security group"
+  description = "First cec"
+  
+
+  tags = {
+    Name = "allow_80"
+  }
+
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+} 
